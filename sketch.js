28676e2ec8b56bb631249f77a1cb54978fa3795d
Afillated <71583904+Afillated@users.noreply.github.com>
2021-03-09 16:59:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,69 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(500, 500);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	bob1 = new Bob(150,260,50)
+	bob2 = new Bob(200,260,50)
+	bob3 = new Bob(250,260,50)
+	bob4 = new Bob(300,260,50)
+	bob5 = new Bob(350,260,50)
+	Ground = new ground(250,100,250,20)
+	rope = new SlingShot(bob1.body ,{x:150,y:110})
+	rope2= new SlingShot(bob2.body ,{x:200,y:110})
+	rope3= new SlingShot(bob3.body ,{x:250,y:110})
+	rope4= new SlingShot(bob4.body ,{x:300,y:110})
+	rope5= new SlingShot(bob5.body ,{x:350,y:110})
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(255,192,203);
+  bob1.display();
+  bob2.display();
+  bob3.display();
+  bob4.display();
+  bob5.display();
+  Ground.display();
+  rope.display();
+  rope2.display();
+  rope3.display();
+  rope4.display();
+  rope5.display();
+  if(bob2.body.speed >2){
+	  Matter.Body.applyForce(bob5.body,bob5.body.position,{x:50,y:-50})
+	
+  }
+  //if(bob4.body.speed >2.5){
+  //  Matter.Body.applyForce(bob1.body,bob1.body.position,{x:-50,y:50})
+	
+  //}
+  drawSprites();
+ 
+}
+function keyPressed(){
+	if(keyCode === UP_ARROW){
+		Matter.Body.applyForce(bob1.body,bob1.body.position,{x:-105,y:-85})
+		
+	}
+	
+	
+}
